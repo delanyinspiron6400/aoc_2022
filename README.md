d5e5da62039844da50ae4c9bec7a07b2da87af62
Martin Winter <martin.winter@huawei.com>
2022-11-30 10:22:17 +0100

    Empty C++ Start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aoc_2022
+# Advent of Code 2022
+
+Holds the code for this years Advent of Code 2022, will try to do it in both C++ and Rust (let's see if I can stick to it).
---
+++ src/main01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "*#*#*# AOC 01.12.2022 *#*#*#\n";
+
+    return 0;
+}
