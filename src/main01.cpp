79a327ff5bd424fc364e85c506b52b28e3972ecb
Martin Winter <martin.winter@icg.tugraz.at>
2022-12-01 06:54:59 +0100

    Add input file for task 1

--- src/main01.cpp
+++ src/main01.cpp
@@ -7,5 +7,22 @@ int main(int argc, char* argv[])
 {
     std::cout << "*#*#*# AOC 01.12.2022 *#*#*#\n";
 
+    std::ifstream file("../input/aoc01.txt");
+
+    auto input_calories{0};
+    using elf_calory_input = std::vector<int>;
+    std::vector<elf_calory_input> calories_per_elf;
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << "---" << line.size() << std::endl;
+        // std::istringstream iss(line);
+        // int a, b;
+        // if (!(iss >> a >> b)) { break; } // error
+
+        // // process pair (a,b)
+    }
+
     return 0;
 }
