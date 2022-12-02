a507c4e4140befa414be0a0d2b077410b1cbd70b
Martin Winter <martin.winter@icg.tugraz.at>
2022-12-02 22:44:08 +0100

    Ranges used for Day 02

--- src/main02.cpp
+++ src/main02.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <ranges>
+#include <algorithm>
 
 
 template<class T>
@@ -25,6 +27,20 @@ int main(int argc, char* argv[])
 
     auto accumulatedScore{0};
     auto accumulatedPredictionScore{0};
+    auto task_1 = parsedInputDataInt | std::views::transform([](const auto val){return (mod(val.second + (1 - val.first), 3) * 3) + (val.second + 1);});
+    auto task_2 = parsedInputDataInt | std::views::transform([](const auto val){return (val.second  * 3) + (mod(val.first + (val.second  - 1), 3) + 1);});
+
+    for(auto&& val : task_1)
+        accumulatedScore += val;
+
+    for(auto&& val : task_2)
+        accumulatedPredictionScore += val;
+
+    std::cout << "Task 1: Predicted Score: " << accumulatedScore << std::endl;
+    std::cout << "Task 2: Predicted Score: " << accumulatedPredictionScore << std::endl;
+    accumulatedScore = 0;
+    accumulatedPredictionScore = 0;
+    
     for(auto [prediction, response] : parsedInputDataInt)
     {
         accumulatedScore += (mod(response + (1 - prediction), 3) * 3) + (response + 1);
