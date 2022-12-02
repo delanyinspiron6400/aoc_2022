64a371e19874d1c41598c9fdc563db0eebd30a74
Martin Winter <martin.winter@icg.tugraz.at>
2022-12-02 06:28:43 +0100

    Start of Day 02

---
+++ aoc02/src/main.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+fn main() {
+    println!("*#*#*# AOC 01.12.2022 *#*#*#");
+    let file_content = fs::read_to_string("../input/aoc01.txt").expect("Could not open file.");
+}
---
+++ src/main02.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "*#*#*# AOC 02.12.2022 *#*#*#\n";
+
+    std::ifstream file("../input/aoc02.txt");
+
+    auto input_calories{0};
+    std::vector<int> caloriesPerElfSum;
+
+    std::string line;
+    auto caloriesSum{0};
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        if ((iss >> input_calories )) 
+        {
+            caloriesSum += input_calories;
+        }
+        else
+        {
+            caloriesPerElfSum.emplace_back(caloriesSum);
+            caloriesSum = 0;
+        }
+    }
+
+    std::sort(caloriesPerElfSum.begin(), caloriesPerElfSum.end(), std::greater<int>());
+
+    std::cout << "Task 1: " << caloriesPerElfSum[0] << " kcal" << std::endl;
+
+    std::cout << "Task 2: " << caloriesPerElfSum[0] + caloriesPerElfSum[1] + caloriesPerElfSum[2]  << " kcal" << std::endl;
+
+    return 0;
+}
