c37a44f2b6c4fa23e50da9ef4a5aa1aa8790c040
Martin Winter <winter.martin@live.at>
2022-12-05 14:24:23 +0100

    Day 05 Task 1

--- src/main05.cpp
+++ src/main05.cpp
@@ -8,13 +8,77 @@
 #include <string_view>
 #include <unordered_set>
 #include <sstream>
+#include <tuple>
+#include <stack>
 
 int main(int argc, char* argv[])
 {
     std::cout << "*#*#*# AOC 05.12.2022 *#*#*#\n";
 
-    std::ifstream header("../input/aoc05_1.txt");
-	std::ifstream moves("../input/aoc05_2.txt");
+    std::ifstream input_stacks("../input/aoc05_1.txt");
+	std::ifstream input_moves("../input/aoc05_2.txt");
+
+    static constexpr int NUM_STACKS{9};
+    std::vector<std::vector<char>> stacks(NUM_STACKS);
+    std::vector<std::tuple<int, int, int>> moves;
+
+    std::string line;
+    std::vector<std::string> stack_strings;
+    while (std::getline(input_stacks, line))
+    {
+        stack_strings.emplace_back(std::move(line));
+    }
+
+    for(auto it = stack_strings.rbegin(); it != stack_strings.rend(); ++it)
+    {
+        for(auto i = 0; i < NUM_STACKS; ++i)
+        {
+            if(it->at(i * 2) != ' ')
+                stacks[i].emplace_back(it->at(i * 2));
+        }
+    }
+
+    while (std::getline(input_moves, line))
+    {
+        std::istringstream iss(line);
+        int move, from, to;
+        iss >> move >> from >> to;
+        moves.emplace_back(move, from - 1, to - 1);
+    }
+
+    // Print stacks
+    for(const auto& stack : stacks)
+    {
+        for(auto c : stack)
+            std::cout << c;
+        std::cout << std::endl;
+    }
+    std::cout << "----------------------\n";
+        
+    for(auto [move, from, to] : moves)
+    {
+        std::cout << "Move " << move << " items from " << from << " to " << to << std::endl;
+        for(auto i {0}; i < move; ++i)
+        {
+            stacks[to].push_back(stacks[from].back());
+            stacks[from].pop_back();
+        }
+        
+        // Print stacks
+        for(const auto& stack : stacks)
+        {
+            for(auto c : stack)
+                std::cout << c;
+            std::cout << std::endl;
+        }
+        std::cout << "----------------------\n";
+    }
+
+    std::cout << "Task 1: ";
+    for(auto& stack : stacks)
+        std::cout << stack.back();
+    std::cout << std::endl;
+
 
 	return 0;
 }
