81462f95e1f04751b8b3d79738eb0cd724a21cb6
Martin Winter <martin.winter@icg.tugraz.at>
2022-12-06 06:31:26 +0100

    Day 06 Task 2

--- src/main06.cpp
+++ src/main06.cpp
@@ -7,14 +7,13 @@
 #include <string_view>
 #include <bitset>
 
-bool checkUniqueness(std::string_view string)
+bool checkUniqueness(std::string_view string, int numUniqueCharacters = 4)
 {
     std::bitset<32> bitmask;
     for(auto c : string)
         bitmask.set(c - 'a');
-    return bitmask.count() == 4;
+    return bitmask.count() == numUniqueCharacters;
 }
-
 
 int main(int argc, char* argv[])
 {
@@ -23,7 +22,6 @@ int main(int argc, char* argv[])
     std::ifstream file("../input/aoc06.txt");
     std::string input;
     std::getline(file, input);
-    std::cout << input << std::endl;
 
     auto startPacketIndex{0};
     for(auto i = 4; i <= input.size(); ++i)
@@ -35,6 +33,17 @@ int main(int argc, char* argv[])
         }
     }
     std::cout << "Task 1 : Start Index at " << startPacketIndex << std::endl;
+
+    auto startMessageIndex{0};
+    for(auto i = 14; i <= input.size(); ++i)
+    {
+        if(checkUniqueness(std::string_view(input.begin() + i - 14, input.begin() + i), 14))
+        {
+            startMessageIndex = i;
+            break;
+        }
+    }
+    std::cout << "Task 2 : Start Index at " << startMessageIndex << std::endl;
 	
 	return 0;
 }
