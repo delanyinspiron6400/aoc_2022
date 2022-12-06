fd4f006c97d07756bf728c2fcada213d2d52de5d
Martin Winter <martin.winter@icg.tugraz.at>
2022-12-06 06:26:32 +0100

    Day 06 Task 1

---
+++ src/main06.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <ranges>
+#include <algorithm>
+#include <string_view>
+#include <bitset>
+
+bool checkUniqueness(std::string_view string)
+{
+    std::bitset<32> bitmask;
+    for(auto c : string)
+        bitmask.set(c - 'a');
+    return bitmask.count() == 4;
+}
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "*#*#*# AOC 06.12.2022 *#*#*#\n";
+
+    std::ifstream file("../input/aoc06.txt");
+    std::string input;
+    std::getline(file, input);
+    std::cout << input << std::endl;
+
+    auto startPacketIndex{0};
+    for(auto i = 4; i <= input.size(); ++i)
+    {
+        if(checkUniqueness(std::string_view(input.begin() + i - 4, input.begin() + i)))
+        {
+            startPacketIndex = i;
+            break;
+        }
+    }
+    std::cout << "Task 1 : Start Index at " << startPacketIndex << std::endl;
+	
+	return 0;
+}
