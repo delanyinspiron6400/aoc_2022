08a29ecc558145200fe6d71f6e7c62b986f61538
Martin Winter <winter.martin@live.at>
2022-12-12 14:10:51 +0100

    Day 12 done

--- src/main12.cpp
+++ src/main12.cpp
@@ -1,21 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
-#include <ranges>
 #include <algorithm>
-#include <string_view>
 #include <bitset>
+#include <fmt/core.h>
+#include <fstream>
+#include <iostream>
 #include <memory>
+#include <queue>
 #include <ranges>
+#include <string>
+#include <string_view>
 #include <unordered_set>
+#include <vector>
 
 #include <range/v3/all.hpp>
-#include <fmt/core.h>
 
 using Input = std::vector<std::vector<int>>;
+static constexpr int INVALID_VALUE{-1};
+auto neighbours{std::vector<std::array<int, 2>>{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
 
 Input ParseInput(std::istream&& input, std::array<int,2>& startIndex, std::array<int,2>& endIndex);
+
+int BFS(const Input & grid, const std::array<int, 2> & startIndex,
+        const std::array<int, 2> & endIndex);
+
+std::vector<std::array<int, 2>> FindStartingPositions(const Input & grid);
 
 int main(int argc, char* argv[])
 {
@@ -27,14 +34,22 @@ int main(int argc, char* argv[])
 
     fmt::print("StartIndex ({}, {}) and EndIndex ({}, {})\n", startIndex[0], startIndex[1], endIndex[0], endIndex[1]);
 
-    // for(auto& line : heightmap)
-    // {
-    //     for(auto height : line)
-    //         std::cout << height << "|";
-    //     std::cout << std::endl;
-    // }
+    auto numSteps{BFS(heightmap, startIndex, endIndex)};
 
-    fmt::print("Task 1 \n");
+    fmt::print("Task 1 : Number of steps needed: {}\n", numSteps);
+
+    auto startingPositions{FindStartingPositions(heightmap)};
+    std::vector<int> routeLenghts;
+    for(auto startingPosition : startingPositions)
+    {
+        auto length{BFS(heightmap, startingPosition, endIndex)};
+        if(length != -1)
+            routeLenghts.emplace_back(length);
+    }
+
+    ranges::sort(routeLenghts);
+
+    fmt::print("Task 2 : Shortest route is: {}\n", routeLenghts[0]);
 
     return 0;
 }
@@ -55,15 +70,89 @@ Input ParseInput(std::istream&& input, std::array<int,2>& startIndex, std::array
             if(c == 'S')
             {
                 startIndex = {rowIndex, columnIndex};
-                return -1;
+                return 0;
             }
             else if (c == 'E')
             {
                 endIndex = {rowIndex, columnIndex};
-                return -2;
+                return 25;
             }
             else
                 return c - 'a'; 
         }) | to<std::vector<int>>; }) |
          to<std::vector<std::vector<int>>>;
+}
+
+bool ValidIndex(const Input & grid, const std::array<int, 2> & index)
+{
+    auto rows{grid.size()};
+    auto columns{grid[0].size()};
+    if(index[0] >= 0 && index[0] < rows && index[1] >= 0 && index[1] < columns)
+        return true;
+    else
+        return false;
+}
+
+int BFS(const Input & grid, const std::array<int, 2> & startIndex,
+        const std::array<int, 2> & endIndex)
+{
+    auto bfsTracker{grid};
+    for(auto & line : bfsTracker)
+        std::fill(line.begin(), line.end(), 0);
+
+    std::queue<std::array<int, 2>> indices;
+    indices.push(startIndex);
+
+    while(indices.size())
+    {
+        auto currentElement{indices.front()};
+        auto currentHeight{grid[currentElement[0]][currentElement[1]]};
+
+        for(auto [row, column] : neighbours)
+        {
+            auto neighbour = currentElement;
+            neighbour[0] += row;
+            neighbour[1] += column;
+            if(ValidIndex(grid, neighbour))
+            {
+                auto height{grid[neighbour[0]][neighbour[1]]};
+                if(height <= (currentHeight + 1))
+                {
+                    if(bfsTracker[neighbour[0]][neighbour[1]] == 0)
+                    {
+                        if(neighbour == endIndex)
+                            return bfsTracker[currentElement[0]][currentElement[1]] + 1;
+                        bfsTracker[neighbour[0]][neighbour[1]] =
+                            bfsTracker[currentElement[0]][currentElement[1]] + 1;
+                        indices.push(neighbour);
+                    }
+                }
+            }
+        }
+
+        indices.pop();
+    }
+
+    return -1;
+}
+
+std::vector<std::array<int, 2>> FindStartingPositions(const Input & grid)
+{
+    // using namespace ranges;
+    using namespace ranges::views;
+    using ranges::getlines;
+    using ranges::to;
+
+    return grid | enumerate | transform([](const auto & line) {
+               auto rowIndex{static_cast<int>(std::get<0>(line))};
+               return std::get<1>(line) | enumerate |
+                      filter([](auto v) { return std::get<1>(v) == 0; }) |
+                      transform([rowIndex](auto val) {
+                          auto columnIndex{static_cast<int>(std::get<0>(val))};
+                          std::array<int, 2> index{rowIndex, columnIndex};
+                          return index;
+                      }) |
+                      to<std::vector<std::array<int, 2>>>;
+           }) |
+           join | to<std::vector<std::array<int, 2>>>;
 }
