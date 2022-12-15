c7215a30e6018451c84c4c0c1b9498976e95e11f
Martin Winter <winter.martin@live.at>
2022-12-15 15:28:01 +0100

    Day 15 parsing

--- src/main15.cpp
+++ src/main15.cpp
@@ -18,9 +18,13 @@
 
 #include <range/v3/all.hpp>
 
-using Input = std::vector<std::vector<int>>;
+using Sensor = std::pair<int, int>;
+using Beacon = std::pair<int, int>;
+using Input = std::vector<std::pair<Sensor, Beacon>>;
 
 Input ParseInput(std::istream&& input);
+
+int CountFieldsWithoutBeacon(const Input & input);
 
 int main(int argc, char* argv[])
 {
@@ -28,10 +32,16 @@ int main(int argc, char* argv[])
 
     auto coords = ParseInput(std::fstream("../input/aoc15.txt"));
 
-    for(auto& line : coords)
+    for(auto & [sensor, beacon] : coords)
     {
-        fmt::print("{} {} {} {}\n", line[0], line[1], line[2], line[3]);
+        auto & [sx, sy]{sensor};
+        auto & [bx, by]{beacon};
+        fmt::print("{} {} {} {}\n", sx, sy, bx, by);
     }
+
+    fmt::print("Task 1: Number of fields without beacon {}\n",
+               CountFieldsWithoutBeacon(coords));
+    fmt::print("Task 2: \n");
 
     return 0;
 }
@@ -43,12 +53,26 @@ Input ParseInput(std::istream && input)
     using ranges::getlines;
     using ranges::to;
 
-    return getlines(input) |
-         transform([](auto& view) { return view | split(' ') | 
-        transform([](auto && rng) 
-            {
-                return std::stoi(std::string(
-                    &*rng.begin(), ranges::distance(rng)));
-            }) | to<std::vector<int>>; }) |
-         to<std::vector<std::vector<int>>>;
+    auto res = getlines(input) | transform([](auto & view) {
+                   return view | split(' ') | transform([](auto && rng) {
+                              return std::stoi(
+                                  std::string(&*rng.begin(), ranges::distance(rng)));
+                          }) |
+                          to<std::vector<int>>;
+               }) |
+               to<std::vector<std::vector<int>>>;
+
+    Input inputParsed;
+    for(auto & line : res)
+    {
+        inputParsed.emplace_back(Sensor{line[0], line[1]}, Beacon{line[2], line[3]});
+    }
+
+    return inputParsed;
+}
+
+int CountFieldsWithoutBeacon(const Input & input)
+{
+
+    return 0;
 }
